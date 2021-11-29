c5d79e29b9af411e863ecdd39c62e8398425307c
sunghyun Kim <constate@naver.com>
2021-11-29 11:59:17 +0900

    .env check

---
+++ src/fbase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+
+// 인증키는 .env 파일에 숨겨두고 gitignore를 통해 github에 공개되지 않도록 설정해야 함
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: "react-firebase-hyun.firebaseapp.com",
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: "react-firebase-hyun.appspot.com",
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
+
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// fbase 불러오기 테스트
+import firebase from './fbase';
+console.log(firebase);
+
 ReactDOM.render(<App />, document.getElementById('root'));
